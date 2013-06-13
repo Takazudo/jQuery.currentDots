c1b3e98983e34f85f383afa234c657e6d9b76ff0
Takeshi Takatsudo <takazudo@gmail.com>
2013-06-13 18:24:08 +0900

    used EveEve as event emitter

--- changelog.md
+++ changelog.md
@@ -1,5 +1,9 @@
 # Change log
 
+## v0.3.0
+
+* used [EveEve](https://github.com/Takazudo/EveEve) as event emitter.
+
 ## v0.2.0
 
 * implemented `class_inactiveItem` option.
--- jquery.currentdots.js
+++ jquery.currentdots.js
@@ -1,89 +1,15 @@
 /*! jQuery.currentDots (https://github.com/Takazudo/jQuery.currentDots)
- * lastupdate: 2013-05-31
+ * lastupdate: 2013-06-13
  * version: 0.2.0
  * author: 'Takazudo' Takeshi Takatsudo <takazudo@gmail.com>
  * License: MIT */
 (function() {
-  var __slice = [].slice,
-    __hasProp = {}.hasOwnProperty,
+  var __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   (function($, window, document) {
     var ns;
     ns = {};
-    ns.Event = (function() {
-
-      function Event() {}
-
-      Event.prototype.on = function(ev, callback) {
-        var evs, name, _base, _i, _len;
-        if (this._callbacks == null) {
-          this._callbacks = {};
-        }
-        evs = ev.split(' ');
-        for (_i = 0, _len = evs.length; _i < _len; _i++) {
-          name = evs[_i];
-          (_base = this._callbacks)[name] || (_base[name] = []);
-          this._callbacks[name].push(callback);
-        }
-        return this;
-      };
-
-      Event.prototype.once = function(ev, callback) {
-        this.on(ev, function() {
-          this.off(ev, arguments.callee);
-          return callback.apply(this, arguments);
-        });
-        return this;
-      };
-
-      Event.prototype.trigger = function() {
-        var args, callback, ev, list, _i, _len, _ref;
-        args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-        ev = args.shift();
-        list = (_ref = this._callbacks) != null ? _ref[ev] : void 0;
-        if (!list) {
-          return;
-        }
-        for (_i = 0, _len = list.length; _i < _len; _i++) {
-          callback = list[_i];
-          if (callback.apply(this, args) === false) {
-            break;
-          }
-        }
-        return this;
-      };
-
-      Event.prototype.off = function(ev, callback) {
-        var cb, i, list, _i, _len, _ref;
-        if (!ev) {
-          this._callbacks = {};
-          return this;
-        }
-        list = (_ref = this._callbacks) != null ? _ref[ev] : void 0;
-        if (!list) {
-          return this;
-        }
-        if (!callback) {
-          delete this._callbacks[ev];
-          return this;
-        }
-        for (i = _i = 0, _len = list.length; _i < _len; i = ++_i) {
-          cb = list[i];
-          if (!(cb === callback)) {
-            continue;
-          }
-          list = list.slice();
-          list.splice(i, 1);
-          this._callbacks[ev] = list;
-          break;
-        }
-        return this;
-      };
-
-      return Event;
-
-    })();
     ns.DotItem = (function(_super) {
 
       __extends(DotItem, _super);
@@ -141,7 +67,7 @@
 
       return DotItem;
 
-    })(ns.Event);
+    })(window.EveEve);
     ns.DotCollection = (function(_super) {
 
       __extends(DotCollection, _super);
@@ -218,7 +144,7 @@
 
       return DotCollection;
 
-    })(ns.Event);
+    })(window.EveEve);
     $.fn.currentDots = function(options) {
       return this.each(function(i, el) {
         var $el, instance;
